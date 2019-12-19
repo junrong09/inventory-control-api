5f2163a12f74ead8ed1ce2d68684b8c0d990262f
junrong09 <junrongy09@gmail.com>
2019-12-19 21:33:54 +0800

    Add exception and meaningful response when request is invalid

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-require('dotenv').config();
+const env = require('dotenv').config();
+if (env.error) {
+  console.log("Environment file (i.e. .env) not found/contains error.");
+  throw env.error;
+}
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
-//const bodyParser = require('body-parser');
 const knex = require('knex');
 const port = 3001;
 
@@ -16,29 +20,41 @@ const db = knex({
   }
 });
 
-//app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
+// Submission API (POST)
 app.post('/submission', (req, res) => {
 	console.log(req.body);
 	const {fixtureId, userId, stocks} = req.body;
 
-	db.insert({fixture_id: fixtureId, employee_id: userId, datetime: new Date().toISOString()}).into('submissions').returning('id').then(ids => insertStocktakes(stocks, ids[0], db));
-	res.send('Successful!');
+	db.insert({fixture_id: fixtureId, employee_id: userId, datetime: new Date().toISOString()})
+    .into('submissions')
+    .returning('id')
+    .then(ids => insertStocktakes(stocks, ids[0], db, res))
+    .catch(err => {
+      console.error(err);
+      res.status(400).send("Fixture ID/user ID does not exist");
+    });
 });
 
+// Test API (GET) 
 app.get('/test', (req, res) => {
   res.send("Connection successful!");
 });
 
-const insertStocktakes = (stocks, submissionId, db) => {
+const insertStocktakes = (stocks, submissionId, db, res) => {
 	let stockRows = [];
 	stocks.forEach(element => {
 		stockRows.push({id: stockRows.length + 1, submission_id: submissionId, upc: element.upc, count: element.count});
 	});
-	console.log(stockRows);
-	db.insert(stockRows).into('stocktakes').then(console.log);
+	db.insert(stockRows)
+    .into('stocktakes')
+    .then(() => res.status(200).send('Successful'))
+    .catch(err => {
+      console.error(err);
+      res.status(400).send("Invalid json format");
+    });
 };
 
 
